8339cf344de6e94ba12060c19f823ee079eae52a
huthifa almnasrah <huthifa.almnasrah@yahoo.com>
2022-05-17 21:48:29 +0300

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+const colors = [
+  "#DAF7A6",
+  "green",
+  "red",
+  "rgba(133,122,200)",
+  "#f15025",
+  "rgba(255, 51, 224)",
+];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
